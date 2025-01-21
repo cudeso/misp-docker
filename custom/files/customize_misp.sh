996afe822e53248a10cd7f9477a838d2bde1ce0e
Koen Van Impe <koen.vanimpe@cudeso.be>
2025-01-21 20:49:34 +0100

    Add logrotate and customise misp script

--- custom/files/customize_misp.sh
+++ custom/files/customize_misp.sh
@@ -2,13 +2,12 @@
 
 # Set correct permissions in misp-custom
 MISP_CUSTOM_DIR="/var/www/MISP/misp-custom"
-chown www-data:www-data "$MISP_CUSTOM_DIR"
+chown -R www-data:www-data "$MISP_CUSTOM_DIR"
 
 # Set correct permissions for cron
 chown root:root /etc/cron.d/misp
 
 
-sudo -H -E -u www-data /var/www/MISP/app/Console/cake Admin setSetting "Security.syslog" true
 sudo -H -E -u www-data /var/www/MISP/app/Console/cake Admin setSetting "MISP.attachments_dir" "/var/www/MISP/app/files"
 sudo -H -E -u www-data /var/www/MISP/app/Console/cake Admin setSetting "Plugin.Action_services_url" "http://misp-modules"
 sudo -H -E -u www-data /var/www/MISP/app/Console/cake Admin setSetting "Plugin.Enrichment_services_url" "http://misp-modules"
@@ -22,3 +21,8 @@ sudo -H -E -u www-data /var/www/MISP/app/Console/cake Admin setSetting "Plugin.Z
 
 sudo -H -E -u www-data /var/www/MISP/app/Console/cake Admin setSetting "MISP.contact" "info@botvrij.eu"
 sudo -H -E -u www-data /var/www/MISP/app/Console/cake Admin setSetting "MISP.email" "info@botvrij.eu"
+
+sudo -H -E -u www-data /var/www/MISP/app/Console/cake Admin setSetting "Security.syslog" true
+sudo -H -E -u www-data /var/www/MISP/app/Console/cake Admin setSetting "Security.syslog_ident" "mispsyslog"
+
+sudo -H -E -u www-data sh -c 'cp /custom/files/edge.css /var/www/MISP/app/webroot/css/edge.css'
